edf9c3418b170d22e67f0220d88e26946e997b0b
heqing <786699892@qq.com>
2017-03-17 11:57:50 +0800

    移除无用代码

--- app/middleware/body.middleware.js
+++ app/middleware/body.middleware.js
@@ -11,7 +11,6 @@ module.exports = function (options) {
                     ctx.post = ctx.request.body.fields;
                     ctx.file = ctx.request.body.files;
                 }
-                resolve();
             });
         });
 
